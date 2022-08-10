0234162e690a63f5064d995806ab2718c37ffa79
Guilherme <guilhermeisk@hotmail.com>
2022-08-10 15:46:39 -0300

    alteração

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 /*
-função anonima no botao que ao clicar irá recuperar e adicionar a propriedade nome do array lista
+função anonima no botao que ao clicar irá recuperar 
+e adicionar a propriedade nome do array lista de forma
+ aleatoria usando o .random() q gera numero aleatorio entre 0 e 1 
+ multiplicando pelo tamanho do array para gerar entre os numeros do array.
 */
 
 let lista = [
@@ -9,5 +12,7 @@ let lista = [
 ]
 
 document.getElementById('button').onclick=function(){
-  document.getElementById('nomes').innerHTML=lista[0].nome
+  let numeroAleatorio=Math.random()*lista.length
+  let arredondamentoParaBaixo=Math.floor(numeroAleatorio)
+  document.getElementById('nomes').innerHTML=lista[arredondamentoParaBaixo].nome
 }
